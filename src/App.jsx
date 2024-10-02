a476c152871639dd37f17d4217ba6bb36930ff62
debian <tl@zasmlabs.com>
2024-10-02 17:27:17 +0530

    react query starter cide

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import { HomePage } from "./components/Home.page";
+import { RQSuperHeroesPage } from "./components/RQSuperHeroes.page";
+import { SuperHeroesPage } from "./components/SuperHeroes.page";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <Router>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/super-heroes">Traditional Super Heroes</Link>
+            </li>
+            <li>
+              <Link to="/rq-super-heroes">RQ Super Heroes</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/super-heroes" element={<SuperHeroesPage />} />
+          <Route path="/rq-super-heroes" element={<RQSuperHeroesPage />} />
+          <Route path="/" element={<HomePage />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
