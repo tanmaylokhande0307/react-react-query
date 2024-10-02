0baa435329b5005bdf5204ca0fb4cfc4b02df87f
debian <tl@zasmlabs.com>
2024-10-02 17:38:57 +0530

    used useQuery

--- src/components/RQSuperHeroes.page.jsx
+++ src/components/RQSuperHeroes.page.jsx
@@ -1,3 +1,22 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+
 export const RQSuperHeroesPage = () => {
-  return <h2>React Query Super Heroes Page</h2>;
+  const {data, isLoading } = useQuery("superHeros", () =>
+    axios.get("http://localhost:4000/superheroes")
+  );
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <>
+      <h2>RQ Super Heroes Page</h2>
+      {console.log(data)}
+      {data?.data.map((hero) => {
+        return <div>{hero.name}</div>;
+      })}
+    </>
+  );
 };
